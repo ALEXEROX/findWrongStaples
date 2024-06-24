49af6eaf2768e30c85e2ba7d0dd411da769c9e2e
ALEXEROX <ermakov.alexerox@yandex.ru>
2024-06-24 17:43:31 +0300

    Main function

--- findWrongStaples/Header.h
+++ findWrongStaples/Header.h
@@ -8,7 +8,7 @@ using namespace std;
 
 /*! Найти неправильно расположенные скобки в коде c++
 * \param[in] code - текст кода
-* \param[out] positions - позиции (номер строки и номер символа) найденных скобок
+* \param[out] positions - позиции (индекс строки и индекс символа) найденных скобок
 * \return количество неправильно расположенных скобок
 */
 int findWrongStaples(vector<string> code, vector<int[2]> positions);
--- findWrongStaples/findWrongStaples.cpp
+++ findWrongStaples/findWrongStaples.cpp
@@ -1,6 +1,63 @@
 ﻿#include "Header.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!\n";
+	// Russioan localization
+	setlocale(LC_ALL, "rus");
+
+	// File names
+	string inputFile = argv[1];
+	string outputFile = argv[2];
+
+	// File streams
+	ifstream input;
+	input.open(inputFile);
+	ofstream output;
+	output.open(outputFile);
+
+	vector<string> code;
+	vector<int[2]> positions;
+
+	// Input
+	string line;
+	while (getline(input, line))
+		code.push_back(line);
+
+	// Execution
+	int wrongStaples = findWrongStaples(code, positions);
+
+	// Output
+	if (!wrongStaples)
+	{
+		output << "Проверка прошла успешно" << endl;
+	}
+	else
+	{
+		output << "Обнаружено " << wrongStaples << " ошиб" << ((wrongStaples % 10 == 1 && wrongStaples % 100 != 11) ? "ка" : ((wrongStaples / 10) % 10) != 1 && (wrongStaples % 10 >= 2 && wrongStaples % 10 <= 4) ? "ки" : "ок") << endl;
+		
+		output << positions[0][0] + 1 << " строка:" << endl;
+		output << code[positions[0][0]] << endl;
+		for (int i = 0; i < positions[0][1]; i++)
+			output << " ";
+		output << "^";
+
+		for (int i = 1; i < wrongStaples; i++)
+		{
+			if (positions[i][0] == positions[i - 1][0])
+			{
+				for (int i = positions[i - 1][1] + 1; i < positions[i][1]; i++)
+					output << " ";
+				output << "^";
+			}
+			else
+			{
+				output << positions[i][0] + 1 << " строка:" << endl;
+				output << code[positions[i][0]] << endl;
+				for (int i = 0; i < positions[i][1]; i++)
+					output << " ";
+				output << "^";
+			}
+		}
+	}
+
 }
